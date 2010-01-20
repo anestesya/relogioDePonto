50c079aab15f835901d9b87a31a630b215a4dfc0
Tadeu Luis <anestesya@gmail.com>
2010-01-20 12:45:32 -0200

    novos métodos no arquivo main.rb

--- main.rb
+++ main.rb
@@ -1,40 +1,50 @@
 #relogio de ponto usado para medir e guardar as horas trabalhadas.
-$horas_por_dia = 8.8
-$horas_na_semana = 44
-$carga_horaria = 0
-puts "escrever uma função que calcula as horas trabalhadas e compara com a hora exigida para a carga horaria."
+$hpd= 8.8 #horas por dia #variaveis criadas com cifrão são variáveis globais.
+$hps = 44 #horas por semana
 
+puts "Cálculo das horas de trabalho."
+puts "by anestesya(aka. Tadeu Luis)"
+
+#calcula o perio que eu trabalhei nesta manhã.
 def periodoDaManha
-  puts "vamos ver quanto vc trabalhou no perído da manhã.\n Digite a hora de entrada:"
+  puts "Digite as informações abaixo:"
+  puts "Hora de entrada: ex: 8.30 para (8:30), 14.20 para (14:20)"
   hora_de_entrada = gets.chomp
-
-  puts "digite a hora do que vc saiu para almoçar."
+  puts "Hora de saída para almoço."
   hora_de_saida_pro_almoco = gets.chomp
 
-   $carga_horaria = hora_de_saida_pro_almoco.to_f-hora_de_entrada.to_f
-   puts "Horas trabalhadas nesta manhã"
-   puts $carga_horaria
+  $chM = getHours(hora_de_entrada, hora_de_saida_pro_almoco)
+  return
 end
 
+def getHours(i, o)
+   he = i.to_f #he = hora de entrada
+   hs = o.to_f #hs = hora de saída
+   puts ch = ho - h1
+   return ch
+end
+
+def calcHours(manha, tarde) 
+   puts "Horas trabalhadas"
+   puts cg = manha +  tarde
+  
+   puts "O total de horas por dia é 8.8:"
+   puts "sua carga horaria hoje foi:"
+   puts cg - $hpd
+end
 
 def periodoDaTarde
    puts "Digite a hora que vc chegou do seu almoço"
    hora_de_entrada_do_almoco = gets.chomp
-
    puts "Digite a hora que vc saiu da empresa após o almoço"
    hora_de_saida_da_empresa = gets.chomp
 
-   $carga_horaria = $carga_horaria + (hora_de_saida_da_empresa.to_f-hora_de_entrada_do_almoco.to_f)
-
-   puts "Horas totais trabalhadas hj"
-   puts $carga_horaria
+   $chT = getHours(hora_de_entrada_do_almoco, hora_de_saida_da_empresa)
+   
+   calcHours($chM, $chT)  
+   return
 end
 
    periodoDaManha
    periodoDaTarde
-
-   puts "Vc trabalhou"
-   puts $carga_horaria
-   puts "hoje, em comparação com o total de horas diarias exigida é: "
-   puts $horas_por_dia.to_f-$carga_horaria.to_f
 
