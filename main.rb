bd5bfd9f9625d662db0cb4362dc56b9a3f1cb80f
Tadeu Luis <anestesya@gmail.com>
2010-01-21 10:04:31 -0200

    métodos reescritos

--- main.rb
+++ main.rb
@@ -1,31 +1,31 @@
 #relogio de ponto usado para medir e guardar as horas trabalhadas.
 
-require 'optparse'
-
 $hpd= 8.8 #horas por dia #variaveis criadas com cifrão são variáveis globais.
 $hps = 44 #horas por semana
 
 #verifica se tem argumentos.
 if !ARGV[0]
-  puts "Você pode tentar usar o programa da seguinte forma:"
-  puts "Use: ruby main.rb hora_entrada hora_saida_almoco hora_entra_almoco hora_sai_empresa"
-  puts "EX: ruby main.rb 8.30 12.30 13.30 18.30"
+  puts "Usage:" 
+  puts "      $ruby main.rb [hora entrada] [hora saida do almoco] [hora entra do almoco] [hora de ir embora]"
+  puts "EX:" 
+  puts "      $ruby main.rb 8.30 12.30 13.30 18.30"
 end
 
-puts "Cálculo das horas de trabalho."
+puts "\nCálculo das horas de trabalho."
 puts "by anestesya(aka. Tadeu Luis)"
 
 #calcula o perio que eu trabalhei nesta manhã.
-def periodoDaManha( arg = {} ) #estou usando hashes para emular polimorfismo http://pt.wikipedia.org/wiki/Ruby_%28linguagem_de_programa%C3%A7%C3%A3o%29
+def periodoDaManha( arg = {} ) #estou usando hashes para emular polimorfismo 
+			       #http://pt.wikipedia.org/wiki/Ruby_%28linguagem_de_programa%C3%A7%C3%A3o%29
   if ARGV[0]
     hora_de_entrada = ARGV[0]
     hora_de_saida_pro_almoco = ARGV[1]
-  elsif
-   puts "Digite as informações abaixo:"
-   puts "Hora de entrada: ex: 8.30 para (8:30), 14.20 para (14:20)"
-   hora_de_entrada = gets.chomp
-   puts "Hora de saída para almoço."
-   hora_de_saida_pro_almoco = gets.chomp
+  else
+   puts ""
+   puts "Digite as horas trabalhadas:"
+   puts "ex: 8.30 para (8:30):"
+   puts "Hora de Entrada: #{hora_de_entrada = gets.chomp}"
+   puts "Hora de saída para almoço.#{hora_de_saida_pro_almoco = gets.chomp}"
  end
 
   $chM = getHours(hora_de_entrada, hora_de_saida_pro_almoco)
@@ -36,28 +36,37 @@ end
 def getHours(i, o)
    he = i.to_f #he = hora de entrada
    hs = o.to_f #hs = hora de saída
-   puts ch = hs - he
+   ch = hs - he
    return ch
 end
 
-def calcHours(manha, tarde) 
-   puts "Horas trabalhadas"
-   puts cg = manha +  tarde
-  
+#calcula todos períodos trabalhados hoje.
+def calcHours(manha, tarde)
+   puts "" 
+   puts "########################### Horas trabalhadas ############################"
    puts "O total de horas por dia é 8.8:"
-   puts "sua carga horaria hoje foi:"
-   puts format("%.2f", cg - $hpd)
+   
+   if (cg-$hpd).to_i == 0
+	puts "Você trabalhou o normalmente hoje, apenas volte para sua casa e descanse. :)"
+   else if (cg-$hpd) < 0.0
+	puts "Cara, amanhã você vai ter que pular da cama para repor esse horário."
+	puts "Vai ficar devendo #{format("%.2f", cg - $hpd)} horas."
+        $horas_repor = format("%.2f", cg - $hpd)
+   else if (cg-$hpd) > 0.0
+        puts "Cara hoje você se esforçou e trabalhou #{format("%.2f", cg - $hpd)} de tempo extra."
+        puts "Se você tiver uma cerveja na sua casa, tome-á ;) "
+        $horas_livres = format("%.2f", cg - $hpd)
+   end
 end
 
 def periodoDaTarde( args={} )
    if ARGV[0]
     hora_de_entrada_do_almoco = ARGV[2]
     hora_de_saida_da_empresa = ARGV[3]
-  elsif
-   puts "Digite a hora que vc chegou do seu almoço"
-   hora_de_entrada_do_almoco = gets.chomp
-   puts "Digite a hora que vc saiu da empresa após o almoço"
-   hora_de_saida_da_empresa = gets.chomp
+  else
+   puts ""
+   puts "Hora do fim do almoço: #{hora_de_entrada_do_almoco = gets.chomp}"
+   puts "Hora da saída pra casa: #{hora_de_saida_da_empresa = gets.chomp}"
   end
   
   $chT = getHours(hora_de_entrada_do_almoco, hora_de_saida_da_empresa)
