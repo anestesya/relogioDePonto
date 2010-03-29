0169f1b86c01afb6df1a39fb88532065e1c594b3
laptop <laptop@laptop.(none)>
2010-03-29 18:10:36 -0300

    linha do tempo funfa parcial

--- main.rb
+++ main.rb
@@ -3,6 +3,8 @@ require 'sinatra'
 require 'twitter'
 require 'oclockrtwit'
 
+#set :environment, :development 
+
 get '/' do
   erb :index
 end
@@ -11,19 +13,35 @@ get '/o_clockr' do
   erb :index
 end
 
+get '/tabelar' do
+  base = logar_o_clockr
+  nome = base.client.username # => 'o_clockr'
+  time_line
+  
+  # http://rdoc.info/projects/jnunemaker/twitter
+  # vamos buscar todas entradas!
+  # esse metodo ta muito bruto, 
+  #   o certo eh armazenar os twit localmente e só roda uns update de tempo em tempo, nao todo carregamento de pagina :P
+  twits = base.home_timeline( :count => 100000 )
+  
+  
+  erb :tabela
+end
+
 enable :sessions
 post '/tweetar' do
-   #httpauth = Twitter::HTTPAuth.new('o_clockr', '1n0d3_50t')
-   #client = Twitter::Base.new(httpauth)
-   oauth = OClockrTwit.new
-   if oauth.consumer
-     oauth.twitter_oauth_login
-     oauth.twitter_oauth_access
-     client = Twitter::Base.new(oauth.Auth)
-   end  
+   # LOGIN
+   client = logar_o_clockr
+   
+   # OAUTH
+   #oauth = OClockrTwit.new
+   #if oauth.consumer
+   #  oauth.twitter_oauth_login
+   #  oauth.twitter_oauth_access
+   #  client = Twitter::Base.new(oauth.Auth)
+   #end  
    
    user = session["user"]
-   
    data = Time.now
    hora = data.strftime("%I:%M%p")
    msg = "#{data.strftime("%a/%b/%y")} - Nesta #{params[:periodo]}, @#{user} #{params[:ponto]} às #{hora}"
@@ -37,9 +55,11 @@ end
 enable :sessions
 post '/oauth' do 
   session["user"] = params[:usuario]
+  debugger
   httpoauth = Twitter::HTTPAuth.new(session["user"], params[:senha])
-  if Twitter::Base.new(httpoauth) 
-     session["user"]
+  tentativa_login = Twitter::Base.new(httpoauth)
+  if tentativa_login
+     session["user"] = tentativa_login
   end
 end
 
@@ -49,4 +69,11 @@ helpers do
     "<li><a href='#{file}' target='_self'>#{filename}</a></li>"
   end
 end
+
+
+def logar_o_clockr
+  httpauth = Twitter::HTTPAuth.new('o_clockr', '1n0d3_50t')
+  Twitter::Base.new(httpauth)
+end
+
 
